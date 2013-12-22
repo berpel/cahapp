885beed77b1810e502dcf2a1bcf516274e531e55
Brett Erpel <berpel@gmail.com>
2013-12-22 15:27:47 -0500

    getting cards

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,8 +14,13 @@ getWhiteCards = function(quantity) {
   var cards = [];
 
   for (var i = 1; i <= quantity; i++) {
-    var card = Math.random() * cardsWhite.length;
+    var card = Math.floor(Math.random() * cardsWhite.length);
+    
+    // get card
     cards.push(cardsWhite[card]);
+
+    // remove card
+    cardsWhite.splice(card, 1)
   }
 
   return cards;
